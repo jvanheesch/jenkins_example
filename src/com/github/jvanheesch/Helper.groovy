8d1578654ee5e77efe27fcbd753d7589cfc28ab8
Joris van Heesch <jvanheesch@gmail.com>
2018-06-18 14:44:11 +0200

    test

--- src/com/github/jvanheesch/Helper.groovy
+++ src/com/github/jvanheesch/Helper.groovy
@@ -2,7 +2,7 @@ package com.github.jvanheesch
 
 class Helper {
     static def mvn(script, args) {
-        script.sh "${script.tool 'Maven'}/bin/mvn -s ${script.env.HOME}/jenkins.xml -o ${args}"
+        script.sh "${script.tool 'maven'}/bin/mvn -s ${script.env.HOME}/jenkins.xml -o ${args}"
     }
 
     static def publishTestResults(script) {
