6a409fcb79dfbe379dadcd5562ce01540294169a
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-06-26 16:25:25 +0200

    slack test

--- src/com/github/jvanheesch/GroovyConfig.groovy
+++ src/com/github/jvanheesch/GroovyConfig.groovy
@@ -1,6 +1,6 @@
 package com.github.jvanheesch
 
 class GroovyConfig implements Serializable {
-    public static final String slackChannel = "#jenkins"
+    public static final String slackChannel = "jenkins"
     public static final String slackColor = "danger"
 }
