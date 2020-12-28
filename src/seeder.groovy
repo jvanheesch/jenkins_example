97c62e3b83d95573ec0bafbef82a33b7a770cf09
jvanheesch <joris.vanheesch@vdab.be>
2020-12-28 18:51:10 +0100

    Disable git tag such that jenkins no longer requires global git config (user.email, user.name)

--- src/seeder.groovy
+++ src/seeder.groovy
@@ -2,7 +2,11 @@ pipelineJob('Generated') {
     definition {
         cpsScm {
             scm {
-                git('https://github.com/jvanheesch/jenkins_example.git')
+                // https://stackoverflow.com/questions/44884121/jenkins-disable-tag-every-build-in-dsl
+                git {
+                    remote { url('https://github.com/jvanheesch/jenkins_example.git') }
+                    extensions {}
+                }
             }
             scriptPath("src/com/github/jvanheesch/release1/alltests/Jenkinsfile")
         }
